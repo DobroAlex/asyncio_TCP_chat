0a589be85355403e0702f97dbdf0f2f69bed01b4
Alex <alexkoshernosiegov@gmail.com>
2019-10-16 00:40:04 +0300

    Added method called by default at user connection

--- AsyncChatV1/server/src/handlers/leading_handler.py
+++ AsyncChatV1/server/src/handlers/leading_handler.py
@@ -4,6 +4,7 @@ from classes import message, message_types_enum
 from .default_text_handler import handle_default_text
 from .user_requset_handler import handle_user_request
 from methods import send_methods, recieve_methods, disconnection_methods
+
 writers = []
 
 
@@ -11,10 +12,9 @@ async def hand(reader, writer):
     writers.append(writer)
     address = writer.get_extra_info('peername')
     print(f'{address} added')
-    greet_msg = message.Message(message_types_enum.MessageTypes.text, 'Server', f'Welcome to server, {address}')
-    await send_methods.send_to_one(writer, writers, greet_msg)
-    notification_message = message.Message(message_types_enum.MessageTypes.text, 'Server', f'{address} joined us')
-    await send_methods.forward_to_all(writer, writers, notification_message)
+
+    await greet_and_notify(writer, writers)
+
     try:
         while True:
             msg = await recieve_methods.receive_message(reader)
@@ -31,3 +31,13 @@ async def hand(reader, writer):
     except ConnectionResetError as e:
         print(f'{address} have disconnect without saying Bye, removing')
         await disconnection_methods.close_writer_forced(writer, writers)
+
+
+async def greet_and_notify(writer, writers: list):
+    address = writer.get_extra_info('peername')
+
+    greet_msg = message.Message(message_types_enum.MessageTypes.text, 'Server', f'Welcome to server, {address}')
+    await send_methods.send_to_one(writer, writers, greet_msg)
+
+    notification_message = message.Message(message_types_enum.MessageTypes.text, 'Server', f'{address} joined us')
+    await send_methods.forward_to_all(writer, writers, notification_message)
