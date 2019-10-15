ad205fffec920efb1571d51543e87fd8c43f4065
Alex <alexkoshernosiegov@gmail.com>
2019-10-15 22:18:26 +0300

    Added logic to handle unexpected connection abortion

--- AsyncChatV1/server/src/handlers/leading_handler.py
+++ AsyncChatV1/server/src/handlers/leading_handler.py
@@ -3,7 +3,7 @@ import jsonpickle
 from classes import message, message_types_enum
 from .default_text_handler import handle_default_text
 from .user_requset_handler import handle_user_request
-from methods import send_methods, recieve_methods
+from methods import send_methods, recieve_methods, disconnection_methods
 writers = []
 
 
@@ -12,19 +12,22 @@ async def hand(reader, writer):
     address = writer.get_extra_info('peername')
     print(f'{address} added')
     greet_msg = message.Message(message_types_enum.MessageTypes.text, 'Server', f'Welcome to server, {address}')
-    await send_methods.send_to_one(writer, greet_msg)
+    await send_methods.send_to_one(writer, writers, greet_msg)
     notification_message = message.Message(message_types_enum.MessageTypes.text, 'Server', f'{address} joined us')
     await send_methods.forward_to_all(writer, writers, notification_message)
+    try:
+        while True:
+            msg = await recieve_methods.receive_message(reader)
 
-    while True:
-        msg = await recieve_methods.receive_message(reader)
+            if not msg:
+                break
 
-        if not msg:
-            break
+            msg = jsonpickle.loads(msg)
 
-        msg = jsonpickle.loads(msg)
-
-        if msg['msg_type'] == message_types_enum.MessageTypes.user_request.value:
-            await handle_user_request(msg['msg'], writer, writers)
-        if msg['msg_type'] == message_types_enum.MessageTypes.text.value:
-            await handle_default_text(msg['msg'], writer, writers)
+            if msg['msg_type'] == message_types_enum.MessageTypes.user_request.value:
+                await handle_user_request(msg['msg'], writer, writers)
+            if msg['msg_type'] == message_types_enum.MessageTypes.text.value:
+                await handle_default_text(msg['msg'], writer, writers)
+    except ConnectionResetError as e:
+        print(f'{address} have disconnect without saying Bye, removing')
+        await disconnection_methods.close_writer_forced(writer, writers)
