762dee7c577d5d6b256b70c4a868d24207b2e37d
Alex <alexkoshernosiegov@gmail.com>
2019-10-15 22:19:03 +0300

    Reworked according to new disconnection methods

--- AsyncChatV1/server/src/handlers/user_requset_handler.py
+++ AsyncChatV1/server/src/handlers/user_requset_handler.py
@@ -12,12 +12,10 @@ async def handle_user_request(msg_text, writer, writers):
     print(msg_text)
     if msg_text == '/exit':
         try:
-            # logger.info(f'{writer}:{addr} removed')
             new_message = message.Message(message_types_enum.MessageTypes.text.value, addr, f'{addr} left us')
             await methods.send_methods.forward_to_all(writer, writers, new_message)
 
-            writer.close()
-            writers.remove(writer)
+            await methods.disconnection_methods.close_writer(writer, writers)
         except Exception as e:
             print(f'{e}')
             pass
@@ -26,7 +24,7 @@ async def handle_user_request(msg_text, writer, writers):
         list_of_users = [w.get_extra_info('peername') for w in writers]
         new_message = message.Message(message_types_enum.MessageTypes.server_response.value, 'Server', f'{list_of_users}')
 
-        await methods.send_methods.send_to_one(writer, new_message)
+        await methods.send_methods.send_to_one(writer, writers, new_message)
     else:
         print(f'{writer}:{addr} request  {msg_text} -- cannot be proceeded')
         # TODO: add respond_to_user ('Uncrossable entity')
