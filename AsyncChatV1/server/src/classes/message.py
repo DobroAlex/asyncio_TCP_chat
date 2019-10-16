214d12f4c6c9494709f61f5aee44efeb439ec96e
DobroAlex <alexkoshernosiegov@gmail.com>
2019-10-16 17:17:33 +0300

    Testing pycharm git

--- AsyncChatV1/server/src/classes/message.py
+++ AsyncChatV1/server/src/classes/message.py
@@ -1,8 +1,8 @@
-import classes.message_types_enum as message_types
+import classes.message_types_enum as message_types_enum
 
 
 class Message:
-    def __init__(self, msg_type: message_types.MessageTypes, author: str, msg: str):
+    def __init__(self, msg_type: message_types_enum.MessageTypes, author: str, msg: str):
         self.msg_type = msg_type
         self.msg = msg
         self.author = author
