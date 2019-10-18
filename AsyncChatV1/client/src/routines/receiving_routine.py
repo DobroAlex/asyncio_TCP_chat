2fe4b21508473f38d038ece5380a9c2e8d508281
Alex <alexkoshernosiegov@gmail.com>
2019-10-18 16:51:09 +0300

    Added special symbols stripping

--- AsyncChatV1/client/src/routines/receiving_routine.py
+++ AsyncChatV1/client/src/routines/receiving_routine.py
@@ -1,8 +1,12 @@
 import socket
-
+import jsonpickle
 
 def receiving_routine(main_socket: socket.socket):
     while True:
         msg = main_socket.recv(1024)
+
         msg = msg.decode('utf-8')
+        msg = msg.replace('\n', ' ').replace('\r', ' ')
+
+        msg = jsonpickle.decode(msg)
         print(msg)
