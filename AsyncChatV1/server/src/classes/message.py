067e1e7347eb5ee0279d3fdd1b77528620ecc602
Alex <alexkoshernosiegov@gmail.com>
2019-10-19 14:06:05 +0300

    Added method to properly deserialize json straight into Message instance instead of dict

--- AsyncChatV1/server/src/classes/message.py
+++ AsyncChatV1/server/src/classes/message.py
@@ -1,5 +1,5 @@
 import dataclasses
-
+import jsonpickle
 import classes.message_types_enum as message_types_enum
 
 
@@ -8,3 +8,7 @@ class Message:
     msg_type: message_types_enum.MessageTypes
     msg: str
     author: str
+
+    @staticmethod
+    def deserialize(json_str: str):
+        return jsonpickle.decode(json_str, classes=Message)
