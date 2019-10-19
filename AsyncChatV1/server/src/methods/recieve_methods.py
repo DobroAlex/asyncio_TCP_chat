909bb4a8279e8a75a2b00b99bfd6225d2000279d
Alex <alexkoshernosiegov@gmail.com>
2019-10-19 16:19:57 +0300

    Adding docs

--- AsyncChatV1/server/src/methods/recieve_methods.py
+++ AsyncChatV1/server/src/methods/recieve_methods.py
@@ -2,5 +2,11 @@ import custom_typing.typing_classes as typing_classes
 
 
 async def receive_message(reader: typing_classes.StreamReader) -> str:
+    """Reads and decodes 1024 bytes (should de enough for chat unless you're sending entire Wiki article.
+
+        :param reader: a reader object that provides APIs to read data from the IO stream.
+
+        :return: Decoded data string (it's expected to be either JSON serialized object or empty string
+    """
     data = await reader.read(1024)
     return data.decode(errors='ignore')
--- AsyncChatV1/server/src/utils.py
+++ AsyncChatV1/server/src/utils.py
@@ -2,8 +2,18 @@ import custom_typing.typing_classes as typing_classes
 
 
 def get_peer_name(writer: typing_classes.StreamWriter) -> tuple:
+    """
+    Returns IP and port of writer
+    :param writer: StreamWriter which IP and port will be returned
+    :return: ("IP", port)
+    """
     return writer.get_extra_info('peername')
 
 
 def get_writer_address(writer: typing_classes.StreamWriter) -> tuple:
+    """
+    Wrapper for get_peer_name()
+    :param writer: Writer which IP and port will be returned
+    :return: ("IP", port)
+    """
     return get_peer_name(writer)
