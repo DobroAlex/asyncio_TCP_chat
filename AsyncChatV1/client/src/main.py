72e6a33ff52fcf7d14ca8cb08c6bafce12ef8ee5
Alex <alexkoshernosiegov@gmail.com>
2019-10-20 23:05:40 +0300

    implemented thread critical thread error detection and halt of all threads in main()

--- AsyncChatV1/client/src/main.py
+++ AsyncChatV1/client/src/main.py
@@ -5,11 +5,21 @@ from routines import establish_connection, receiving_routine, forwarding_routine
 
 def main():
     main_socket = establish_connection.establish_connection('127.0.0.1', 8088)
-    receiving = threading.Thread(target=receiving_routine.receiving_routine, args=(main_socket,))
-    receiving.start()
 
-    sending_routine = threading.Thread(target=forwarding_routine.forwarding_routine, args=(main_socket,))
-    sending_routine.start()
+    receiving = threading.Thread(target=receiving_routine.receiving_routine, args=(main_socket,), daemon=True)
+
+    sending = threading.Thread(target=forwarding_routine.forwarding_routine, args=(main_socket,), daemon=True)
+
+    running_threads = (receiving, sending)
+
+    for thread in running_threads:
+        thread.start()
+
+    while True:
+        for thread in running_threads:
+            if not thread.is_alive():
+                print(f'Thread {thread.name} is dead, aborting execution')
+                return
 
 
 if __name__ == '__main__':
