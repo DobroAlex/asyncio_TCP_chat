f2e4d2effdbe69cbd29cb2e939d0a07d8bb38ace
Alex <alexkoshernosiegov@gmail.com>
2019-10-20 18:52:02 +0300

    Added plaint text message receipt

--- AsyncChatV1/client/src/routines/receiving_routine.py
+++ AsyncChatV1/client/src/routines/receiving_routine.py
@@ -1,12 +1,19 @@
 import socket
 import jsonpickle
 
+from classes import message, message_types_enum as message_types
+
 def receiving_routine(main_socket: socket.socket):
     while True:
         msg = main_socket.recv(1024)
 
-        msg = msg.decode('utf-8')
+        msg = msg.decode('utf-8', errors='ignore')
         msg = msg.replace('\n', ' ').replace('\r', ' ')
 
         msg = jsonpickle.decode(msg)
-        print(msg)
+
+        if type(msg) == dict:
+            msg = message.Message.create_from_dict(msg)
+
+        if msg.msg_type == message_types.MessageTypes.text.value:
+            print(f'{msg.author}: {msg.msg}')
