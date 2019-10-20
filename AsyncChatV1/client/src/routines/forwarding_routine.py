dc6da92871576759801e6f4a9ab6901ae5c58682
Alex <alexkoshernosiegov@gmail.com>
2019-10-20 21:28:31 +0300

    Added simple  error handling

--- AsyncChatV1/client/src/routines/forwarding_routine.py
+++ AsyncChatV1/client/src/routines/forwarding_routine.py
@@ -1,14 +1,26 @@
 import jsonpickle
+import json
+
+import socket
+
+
 from classes import message, message_types_enum as message_types
 
 
-def forwarding_routine(main_socket):
+def forwarding_routine(main_socket: socket.socket):
     while True:
-        raw_input = input()
+        try:
+            raw_input = input()
 
-        msg = message.Message(msg=raw_input, author='', msg_type=message_types.MessageTypes.text.value)
+            msg = message.Message(msg=raw_input, author='', msg_type=message_types.MessageTypes.text.value)
 
-        msg = jsonpickle.encode(msg)
-        msg = bytearray(msg, 'utf-8')
+            msg = jsonpickle.encode(msg)
+            msg = bytearray(msg, 'utf-8')
 
-        main_socket.send(msg)
+            main_socket.send(msg)
+
+        except (ConnectionAbortedError, ConnectionResetError, ConnectionError, ConnectionRefusedError) as e:
+            print('It seems that server is dead. You should try to restart the program.')
+            print(f'{e}')
+
+            break
