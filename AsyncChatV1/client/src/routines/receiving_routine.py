8e37e01995aaa9948ac373ca721325d8b9160c7a
Alex <alexkoshernosiegov@gmail.com>
2019-10-20 21:10:32 +0300

    Added simple error handling inside receiving loop

--- AsyncChatV1/client/src/routines/receiving_routine.py
+++ AsyncChatV1/client/src/routines/receiving_routine.py
@@ -1,20 +1,32 @@
 import socket
 import jsonpickle
+import json
 
 from classes import message, message_types_enum as message_types
 
 
 def receiving_routine(main_socket: socket.socket):
     while True:
-        msg = main_socket.recv(1024)
+        try:
+            msg = main_socket.recv(1024)
 
-        msg = msg.decode('utf-8', errors='ignore')
-        msg = msg.replace('\n', ' ').replace('\r', ' ')
+            msg = msg.decode('utf-8', errors='ignore')
+            msg = msg.replace('\n', ' ').replace('\r', ' ')
 
-        msg = jsonpickle.decode(msg)
+            msg = jsonpickle.decode(msg)
 
-        if type(msg) == dict:
-            msg = message.Message.create_from_dict(msg)
+            if type(msg) == dict:
+                msg = message.Message.create_from_dict(msg)
 
-        if msg.msg_type == message_types.MessageTypes.text.value:
-            print(f'{msg.author}: {msg.msg}')
+            if msg.msg_type == message_types.MessageTypes.text.value:
+                print(f'{msg.author}: {msg.msg}')
+
+        except json.JSONDecodeError:  # This should NOT occur normally
+            print(f'SYS_WARNING: Wow, server is sending garbage. That\'s unusual: f{msg}')
+
+        except (ConnectionAbortedError, ConnectionResetError, ConnectionError, ConnectionRefusedError) as e:
+            print('It seems that server is dead. You should try to restart the program.')
+            print(f'{e}')
+
+            break
+
