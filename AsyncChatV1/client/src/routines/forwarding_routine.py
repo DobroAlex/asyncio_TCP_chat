c6ef5122f3b4ac17e3c34075519aafcee14efa5e
Alex <alexkoshernosiegov@gmail.com>
2019-10-20 19:01:56 +0300

    Added very simple sending routine

--- AsyncChatV1/client/src/routines/forwarding_routine.py
+++ AsyncChatV1/client/src/routines/forwarding_routine.py
@@ -1,5 +1,14 @@
+import jsonpickle
+from classes import message, message_types_enum as message_types
+
+
 def forwarding_routine(main_socket):
     while True:
-        msg = input()
+        raw_input = input()
+
+        msg = message.Message(msg=raw_input, author='', msg_type=message_types.MessageTypes.text.value)
+
+        msg = jsonpickle.encode(msg)
         msg = bytearray(msg, 'utf-8')
+
         main_socket.send(msg)
