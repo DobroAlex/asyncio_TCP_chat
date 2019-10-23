7094fda577db3bd4eb360633ce66b2546547d7dd
Alex <alexkoshernosiegov@gmail.com>
2019-10-23 17:58:04 +0300

    Added logger creation function

--- AsyncChatV1/server/src/utils.py
+++ AsyncChatV1/server/src/utils.py
@@ -1,3 +1,6 @@
+import sys
+import logging
+
 import custom_typing.typing_classes as typing_classes
 
 
@@ -17,3 +20,17 @@ def get_writer_address(writer: typing_classes.StreamWriter) -> tuple:
     :return: ("IP", port)
     """
     return get_peer_name(writer)
+
+
+def create_file_logger(file_name: str, stream=sys.stdout, level=logging.INFO) -> logging.Logger:
+    """
+    Creates specific logger for given file. It's expected that this function will be called once per each file
+    :param file_name: best way to get one -- use __name__
+    :param stream: output stream for logging. Normal stdout by default
+    :param level: default logging level
+    :return: new logging object
+    """
+    # https://stackoverflow.com/questions/15727420/using-python-logging-in-multiple-modules
+    logging.basicConfig(stream=stream, level=level)
+    file_name_stripped = file_name.replace('__', ' ')
+    return logging.getLogger(file_name_stripped)
